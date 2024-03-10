bc5603233473a27d3265b8e5cdea2f87b110c970
Atte Oksanen <atteoksanen@outlook.com>
2024-03-10 23:18:30 +0200

    added failsafe for no data

--- teiden_talvikunnossapito_CRON/src/services/roadDataUpdate.js
+++ teiden_talvikunnossapito_CRON/src/services/roadDataUpdate.js
@@ -1,32 +1,35 @@
 const PlowData = require('../models/plowData')
 
 const updateRoadData = async (inputPlowData) => {
-    const startTime = Date.now()
-    const inputData = inputPlowData.geoJson.features
-    const roadNamesWithTime = new Map()
-    inputData.forEach(element => {
-        if (roadNamesWithTime.has(element.properties.roadName)) {
-            roadNamesWithTime.set(element.properties.roadName, { time: Math.max(element.properties.time, roadNamesWithTime.get(element.properties.roadName).time), coords: roadNamesWithTime.get(element.properties.roadName).coords })
-        } else {
-            roadNamesWithTime.set(element.properties.roadName, { time: element.properties.time, coords: element.geometry.coordinates[0] })
-        }
-    })
+  const startTime = Date.now()
+  const inputData = inputPlowData.geoJson.features
+  if (inputData.length < 1) {
+    return
+  }
+  const roadNamesWithTime = new Map()
+  inputData.forEach(element => {
+    if (roadNamesWithTime.has(element.properties.roadName)) {
+      roadNamesWithTime.set(element.properties.roadName, { time: Math.max(element.properties.time, roadNamesWithTime.get(element.properties.roadName).time), coords: roadNamesWithTime.get(element.properties.roadName).coords })
+    } else {
+      roadNamesWithTime.set(element.properties.roadName, { time: element.properties.time, coords: element.geometry.coordinates[0] })
+    }
+  })
 
 
-    const roadNameArray = Array.from(roadNamesWithTime)
-    const bulkOperations = roadNameArray.map(element => {
-        return {
-            updateOne: {
-                filter: { roadName: element[0] },
-                update: { $set: { roadName: element[0], timeStamp: element[1].time, coords: element[1].coords } },
-                upsert: true
-            }
-        }
-    })
-    const bulkOpResult = await PlowData.collection.bulkWrite(bulkOperations)
-    console.log('Bulk operation finished in', (Date.now() - startTime) / 1000, 'seconds')
-    console.log(`Updated plowing data for ${bulkOpResult.modifiedCount} roads`)
-    console.log(`Added plowing data for ${bulkOpResult.upsertedCount} roads`)
+  const roadNameArray = Array.from(roadNamesWithTime)
+  const bulkOperations = roadNameArray.map(element => {
+    return {
+      updateOne: {
+        filter: { roadName: element[0] },
+        update: { $set: { roadName: element[0], timeStamp: element[1].time, coords: element[1].coords } },
+        upsert: true
+      }
+    }
+  })
+  const bulkOpResult = await PlowData.collection.bulkWrite(bulkOperations)
+  console.log('Bulk operation finished in', (Date.now() - startTime) / 1000, 'seconds')
+  console.log(`Updated plowing data for ${bulkOpResult.modifiedCount} roads`)
+  console.log(`Added plowing data for ${bulkOpResult.upsertedCount} roads`)
 }
 
 module.exports = updateRoadData
